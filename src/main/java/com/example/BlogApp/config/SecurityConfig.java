bec9aa227c736d3aa6a9d8828bd6af6a4356e982
Tram Lai <hongoctram17@gmail.com>
2024-09-03 22:53:10 +0700

    add react and security config

--- src/main/java/com/example/BlogApp/config/SecurityConfig.java
+++ src/main/java/com/example/BlogApp/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.example.BlogApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig   {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
